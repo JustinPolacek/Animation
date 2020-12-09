4f9ef307219f61b9c4004b76ab33c8ed858007bb
justin Polacek <justinpolacek@gmail.com>
2020-12-09 08:48:37 -0800

    commited

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const tl = gsap.timeline({defaults: {ease: "power1.out"}});
 
-tl.to('.text', {y:"0%", duration: 1, stagger: 0.25});
-tl.to('.slider', {y: "-100%", duration: 1.5, delay: 0.5});
-tl.to('.intro', {y: "-100%", duration: 1}, "-=1");
+tl.to('.text', {y:"0%", duration: 1.5, stagger: 0.75});
+tl.to('.text-2', {y:"0%", duration: 1.5, duration: 0.75});
+tl.to('.text-3', {y:"0%", duration: 1.5, duration: 0.75});
+tl.to('.slider', {x: "-100%", duration: 1.5, delay: 0.5});
+tl.to('.intro', {x: "-100%", duration: 1},"-=1");
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1})
 tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
